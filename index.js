1d1e5eecf6f31111a56c80457dcde05b794be735
Nhat Linh <nhatlinh161232@gmail.com>
2021-07-07 10:31:40 +1000

    update res

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ bye: "buddy" });
 });
 
 const PORT = process.env.PORT || 5000;
