a451909245436601e7b959a24eb33318aa6f8095
Nhat Linh <nhatlinh161232@gmail.com>
2021-07-08 15:04:47 +1000

    refactor code

---
+++ routes/authRoutes.js
@@ -0,0 +1,13 @@
+/** @format */
+const passport = require("passport");
+
+module.exports = (app) => {
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
+    })
+  );
+
+  app.get("/auth/google/callback", passport.authenticate("google"));
+};
