b22d3af85f92cec9a4811446a073f358191ea017
Nhat Linh <nhatlinh161232@gmail.com>
2021-07-10 23:04:15 +1000

    first model for mongoDB

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
+require("./models/User");
 require("./services/passport");
+
 mongoose
   .connect(keys.mongoURI, {
     useNewUrlParser: true,
--- services/passport.js
+++ services/passport.js
@@ -2,6 +2,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
+const mongoose = require("mongoose");
+
+const User = mongoose.model("users");
 
 passport.use(
   new GoogleStrategy(
@@ -11,9 +14,16 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("access token", accessToken);
-      console.log("refresh token", refreshToken);
-      console.log("profile", profile);
+      User.findOne({
+        googleId: profile.id,
+      }).then((existingUser) => {
+        if (existingUser) {
+        } else {
+          new User({
+            googleId: profile.id,
+          }).save();
+        }
+      });
     }
   )
 );
