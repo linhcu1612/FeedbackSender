b7e0e14b034f88bb14f2cc8dd901c95b04bf5a69
Nhat Linh <nhatlinh161232@gmail.com>
2021-07-11 11:54:55 +1000

    serializeUser

--- services/passport.js
+++ services/passport.js
@@ -6,6 +6,17 @@ const mongoose = require("mongoose");
 
 const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+  done(null, user);
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -18,10 +29,13 @@ passport.use(
         googleId: profile.id,
       }).then((existingUser) => {
         if (existingUser) {
+          done(null, existingUser);
         } else {
           new User({
             googleId: profile.id,
-          }).save();
+          })
+            .save()
+            .then((user) => done(null, user));
         }
       });
     }
