d9eaffaa6c1e3fb7fc720dd5a24d46e4c3d784a5
Nhat Linh <nhatlinh161232@gmail.com>
2021-07-12 09:34:22 +1000

    add in logout route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,6 +9,11 @@ module.exports = (app) => {
     })
   );
 
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
   app.get("/auth/google/callback", passport.authenticate("google"));
 
   app.get("/api/current_user", (req, res) => {
