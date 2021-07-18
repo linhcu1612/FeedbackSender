f47a17e5a8db36a7cd4bf0ab53d4a90bd82e984f
Nhat Linh <nhatlinh161232@gmail.com>
2021-07-18 21:09:08 +1000

    add auth router

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -17,6 +17,7 @@ module.exports = (app) => {
   app.get("/auth/google/callback", passport.authenticate("google"));
 
   app.get("/api/current_user", (req, res) => {
+    console.log(req.user);
     res.send(req.user);
   });
 };
